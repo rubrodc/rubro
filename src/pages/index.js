99973ed8b4da945f74198920a164c12a9afd3878
Rúben Rodrigues <80220701+rubrodc@users.noreply.github.com>
2022-09-19 23:44:36 +0100

    work updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,36 +34,37 @@ function Home() {
 
         <div className="row">
           <div className="column">
-            <img className="headerphoto1" src={useBaseUrl("https://images.ctfassets.net/2d5q1td6cyxq/Ce2C5fxlv2tgXJx7R4tn9/4d602ef12626a064e665b96250094f37/PD02285_-_floating_Furniture_Maker.png?fm=jpg&q=85")}/>
-            <img className="headerphoto2" src={useBaseUrl("https://images.unsplash.com/photo-1558655146-d09347e92766?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80")}/>
-            <img className="headerphoto3" src={useBaseUrl("https://images.unsplash.com/photo-1558591710-4b4a1ae0f04d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80")}/>
-            <img className="headerphoto4" src={useBaseUrl("https://images.ctfassets.net/2d5q1td6cyxq/Ce2C5fxlv2tgXJx7R4tn9/4d602ef12626a064e665b96250094f37/PD02285_-_floating_Furniture_Maker.png?fm=jpg&q=85")}/>
-            <img className="headerphoto5" src={useBaseUrl("https://images.ctfassets.net/2d5q1td6cyxq/2RuM54HIeuFO3WGmDJYesF/9d977211b63375cd3ad18410833ed7fd/PD02285_-_floating_fsr_laptop.png?fm=jpg&q=85")}/>
-            <img className="headerphoto6" src={useBaseUrl("https://images.ctfassets.net/2d5q1td6cyxq/Ce2C5fxlv2tgXJx7R4tn9/4d602ef12626a064e665b96250094f37/PD02285_-_floating_Furniture_Maker.png?fm=jpg&q=85")}/>
+            <img className="headerphoto1" src={useBaseUrl("/img/burble.gif")}/>
+            <img className="headerphoto2" src={useBaseUrl("/img/bikeicon.jpg")}/>
+            <img className="headerphoto3" src={useBaseUrl("/img/arrows.jpg")}/>
+            <img className="headerphoto4" src={useBaseUrl("/img/36dot.jpg")}/>
+            <img className="headerphoto5" src={useBaseUrl("/img/qp.jpg")}/>
+            <img className="headerphoto6" src={useBaseUrl("/img/25abril.jpg")}/>
           </div>
           <div className="column">
-            <img className="headerphoto1" src={useBaseUrl("https://images.unsplash.com/photo-1533122250115-6bb28e9a48c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80")}/>
-            <img className="headerphoto2" src={useBaseUrl("https://images.ctfassets.net/2d5q1td6cyxq/Ce2C5fxlv2tgXJx7R4tn9/4d602ef12626a064e665b96250094f37/PD02285_-_floating_Furniture_Maker.png?fm=jpg&q=85")}/>
-            <img className="headerphoto3" src={useBaseUrl("https://images.unsplash.com/photo-1506792006437-256b665541e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80")}/>
-            <img className="headerphoto4" src={useBaseUrl("https://images.ctfassets.net/2d5q1td6cyxq/Ce2C5fxlv2tgXJx7R4tn9/4d602ef12626a064e665b96250094f37/PD02285_-_floating_Furniture_Maker.png?fm=jpg&q=85")}/>
-            <img className="headerphoto5" src={useBaseUrl("https://images.ctfassets.net/2d5q1td6cyxq/2RuM54HIeuFO3WGmDJYesF/9d977211b63375cd3ad18410833ed7fd/PD02285_-_floating_fsr_laptop.png?fm=jpg&q=85")}/>
-            <img className="headerphoto6" src={useBaseUrl("https://images.ctfassets.net/2d5q1td6cyxq/Ce2C5fxlv2tgXJx7R4tn9/4d602ef12626a064e665b96250094f37/PD02285_-_floating_Furniture_Maker.png?fm=jpg&q=85")}/>
+            <img className="headerphoto1" src={useBaseUrl("/img/palm-tree.png")}/>
+            <img className="headerphoto2" src={useBaseUrl("/img/eb.jpg")}/>
+            <img className="headerphoto3" src={useBaseUrl("/img/portal.gif")}/>
+            <img className="headerphoto4" src={useBaseUrl("/img/tek.jpg")}/>
+            <img className="headerphoto5" src={useBaseUrl("/img/paloma.jpg")}/>
+            <img className="headerphoto6" src={useBaseUrl("/img/yingyang.png")}/>
           </div>
           <div className="column">
-            <img className="headerphoto1" src={useBaseUrl("")}/>
-            <img className="headerphoto2" src={useBaseUrl("https://images.ctfassets.net/2d5q1td6cyxq/Ce2C5fxlv2tgXJx7R4tn9/4d602ef12626a064e665b96250094f37/PD02285_-_floating_Furniture_Maker.png?fm=jpg&q=85")}/>
-            <img className="headerphoto3" src={useBaseUrl("https://images.ctfassets.net/2d5q1td6cyxq/1QXEwttBcJmqTueBfSB2Ri/226b3f966f1452c0a9a6e206fc4aa9c0/PD02285_-_floating_landscaper.png?fm=jpg&q=85")}/>
 
-            <img className="headerphoto5" src={useBaseUrl("https://images.ctfassets.net/2d5q1td6cyxq/2RuM54HIeuFO3WGmDJYesF/9d977211b63375cd3ad18410833ed7fd/PD02285_-_floating_fsr_laptop.png?fm=jpg&q=85")}/>
-            <img className="headerphoto6" src={useBaseUrl("https://images.ctfassets.net/2d5q1td6cyxq/Ce2C5fxlv2tgXJx7R4tn9/4d602ef12626a064e665b96250094f37/PD02285_-_floating_Furniture_Maker.png?fm=jpg&q=85")}/>
+            <img className="headerphoto1" src={useBaseUrl("/img/looking.gif")}/>
+            <img className="headerphoto2" src={useBaseUrl("/img/flower.png")}/>
+            <img className="headerphoto3" src={useBaseUrl("/img/ampersand.png")}/>
+            <img className="headerphoto4" src={useBaseUrl("/img/mars2020.jpg")}/>
+            <img className="headerphoto5" src={useBaseUrl("/img/100.png")}/>
+            <img className="headerphoto6" src={useBaseUrl("/img/cs.png")}/>
           </div>
           <div className="column">
-            <img className="headerphoto1" src={useBaseUrl("https://images.ctfassets.net/2d5q1td6cyxq/Ce2C5fxlv2tgXJx7R4tn9/4d602ef12626a064e665b96250094f37/PD02285_-_floating_Furniture_Maker.png?fm=jpg&q=85")}/>
-            <img className="headerphoto2" src={useBaseUrl("https://images.ctfassets.net/2d5q1td6cyxq/Ce2C5fxlv2tgXJx7R4tn9/4d602ef12626a064e665b96250094f37/PD02285_-_floating_Furniture_Maker.png?fm=jpg&q=85")}/>
-            <img className="headerphoto3" src={useBaseUrl("")}/>
-            <img className="headerphoto4" src={useBaseUrl("https://images.ctfassets.net/2d5q1td6cyxq/Ce2C5fxlv2tgXJx7R4tn9/4d602ef12626a064e665b96250094f37/PD02285_-_floating_Furniture_Maker.png?fm=jpg&q=85")}/>
-            <img className="headerphoto5" src={useBaseUrl("https://images.ctfassets.net/2d5q1td6cyxq/2RuM54HIeuFO3WGmDJYesF/9d977211b63375cd3ad18410833ed7fd/PD02285_-_floating_fsr_laptop.png?fm=jpg&q=85")}/>
-            <img className="headerphoto6" src={useBaseUrl("https://images.ctfassets.net/2d5q1td6cyxq/Ce2C5fxlv2tgXJx7R4tn9/4d602ef12626a064e665b96250094f37/PD02285_-_floating_Furniture_Maker.png?fm=jpg&q=85")}/>
+            <img className="headerphoto1" src={useBaseUrl("/img/wallpaper.png")}/>
+            <img className="headerphoto2" src={useBaseUrl("/img/bloomstone.jpg")}/>
+            <img className="headerphoto3" src={useBaseUrl("/img/diamondhands.jpg")}/>
+            <img className="headerphoto4" src={useBaseUrl("/img/plus.gif")}/>
+            <img className="headerphoto5" src={useBaseUrl("/img/greative.jpg")}/>
+            <img className="headerphoto6" src={useBaseUrl("/img/spread-love.jpg")}/>
           </div>
 
         </div>
